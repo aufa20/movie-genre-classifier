0bc5b2e103775cfbd4e4b872d4e0a495316f5a0a
aufa20 <0221123290@student.kuips.edu.my>
2025-07-05 03:32:50 +0800

    Enable CORS for frontend access

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import fasttext
 
 app = Flask(__name__)
+CORS(app)
 
 # Load FastText model
 model = fasttext.load_model("movie_genre_model.ftz")
